6393c7194d9ca8ec0e2f20b6f4ca1a6cd8ca859b
Pablo Benito <biongbenito@gmail.com>
2017-04-03 18:09:13 -0300

    Put a sample react component

--- app/app.js
+++ app/app.js
@@ -1,2 +1,17 @@
 'use strict'
-console.log('¡¡¡******************01 Hello World *********************');
+console.log('¡¡¡****************** 01 Hello World *********************');
+
+import React, {Component } from 'react';//https://github.com/facebook/react/issues/2607#issuecomment-225911048
+import ReactDOM from 'react-dom';
+
+class Hello extends Component {
+    render () {
+        return (
+            <div>
+                01 Hello React!
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('container'));
